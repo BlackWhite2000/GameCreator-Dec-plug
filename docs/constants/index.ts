e54527449245324dd49527927d2495c09bda9bb0
黑白 <2285697543@qq.com>
2023-12-24 19:43:21 +0800

    docs

--- docs/constants/index.ts
+++ docs/constants/index.ts
@@ -1,5 +1,5 @@
-const app = '名称'
-const appType = '类型'
+const app = '解谜功能'
+const appType = '插件'
 const appTypeId = 0
 const appName = `${app}${appType}文档`
 const appDescription = `${app}${appType}文档`
